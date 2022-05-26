6bf8540aed3fa5fe29ce4ca08ccc1a00841cafaa
KCLee <choi36412760@gmail.com>
2022-05-26 10:17:19 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,61 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "以下何者為計算 $\\widehat{AB}$ 的長度的正確公式？",
+    answer: "optionC",
+    options: [
+      "$$\\widehat{AB}=(π×6×\\frac{150°}{360°}) \\textrm{ cm}$$",
+      "$$\\widehat{AB}=(π×6^2×\\frac{150°}{360°}) \\textrm{ cm}$$",
+      "$$\\widehat{AB}=(2π×6×\\frac{150°}{360°}) \\textrm{ cm}$$",
+      "$$\\widehat{AB}=(2π×6×\\frac{1}{3}) \\textrm{ cm}$$"
+    ]
+  },
+    {
+    numb: 2,
+    question: "以下何者為計算 $\\widehat{AB}$ 的長度的正確公式？",
+    answer: "optionD",
+    options: [
+      "$$\\widehat{AB}=(π×10^2×\\frac{360°-288°}{360°}) \\textrm{ cm}$$",
+      "$$\\widehat{AB}=(2π×10×\\frac{360°-288°}{360°}) \\textrm{ cm}$$",
+      "$$\\widehat{AB}=(π×10^2×\\frac{288°}{360°}) \\textrm{ cm}$$",
+      "$$\\widehat{AB}=(2π×10×\\frac{288°}{360°}) \\textrm{ cm}$$"
+    ]
+  },
+    {
+    numb: 3,
+    question: "下列哪條方程能夠求得 $r$ 的值？",
+    answer: "optionB",
+    options: [
+      "$$πr^2×\\frac{216°}{360°}=18π$$",
+      "$$2πr×\\frac{216°}{360°}=18π$$",
+      "$$πr^2×\\frac{216°}{360°}=18$$",
+      "$$2πr×\\frac{216°}{360°}=18$$"
+    ]
+  },
+    {
+    numb: 4,
+    question: "下列哪條方程能夠求得 <i>θ</i> 的值？",
+    answer: "optionA",
+    options: [
+      "$$2π×30×\\frac{θ}{360°}=27π$$",
+      "$$π×30^2×\\frac{θ}{360°}=27π$$",
+      "$$2π×30÷\\frac{θ}{360°}=27π$$",
+      "$$π×30^2÷\\frac{θ}{360°}=27π$$"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
